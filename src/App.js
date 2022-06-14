80532c10541424dca6708ca01b37ebe33f3b6d0a
unknown <richardmaichs276@gmail.com>
2022-06-13 22:10:46 -0400

    created initialized components, homepage, header, etc

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import TopBar from "./topbar/Topbar";
+import Home from "./pages/home/Home";
+
+function App() {
+  return (
+    <>
+    <TopBar/>
+    <Home/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/topbar/Topbar.jsx
@@ -0,0 +1,31 @@
+import './topbar.css'
+import mainPhoto from'./imgs/mainPhoto.png'
+
+export default function Topbar() {
+  return (
+    <div className="top">
+        <div className="topLeft">
+        <i className="topIcon fa-brands fa-facebook-square"></i>
+        <i className="topIcon fa-brands fa-twitter-square"></i>
+        <i className="topIcon fa-brands fa-linkedin"></i>
+        <i className="topIcon fa-brands fa-instagram-square"></i>
+    </div>
+
+        <div className="topCenter">
+            <ul className="topList">
+            <li className="topListItem">HOME</li>
+            <li className="topListItem">ABOUT</li>
+            <li className="topListItem">CONTACT</li>
+            <li className="topListItem">WRITE</li>
+            <li className="topListItem">LOGOUT</li>
+            </ul>
+        </div>
+        <div className="topRight">
+            <img src= {mainPhoto}
+            className="topImg" 
+             alt="error" />
+        <i className=" topSearchIcon fa-solid fa-magnifying-glass"></i>
+        </div>
+    </div>
+  )
+}
